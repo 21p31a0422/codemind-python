301a144830febc13b7548168a254b0da457b9443
Aditya Guthula <99874143+21p31a0422@users.noreply.github.com>
2023-05-31 22:26:57 +0530

    n = int(input()) arr = list(map(int, input(). split())) a, b = map(int, input(). split()) total = sum([i for i in arr if a <= i <= b]) print(total)

--- sum_of_elements_between_A_and_B.py
+++ sum_of_elements_between_A_and_B.py
@@ -1,8 +1,5 @@
 n = int(input())
 arr = list(map(int, input(). split()))
 a, b = map(int, input(). split())
-total = 0
-for i in arr:
-    if a <= i <= b:
-        total += i
+total = sum([i for i in arr if a <= i <= b])
 print(total)
